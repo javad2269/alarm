ba83a5eb739d3207a78eb1375f7e0c60bf2819ce
javad2269 <155516010+javad2269@users.noreply.github.com>
2026-08-23 12:10:56 +0330

    Create StopAlarmReceiver.java

---
+++ android/app/src/main/java/app/alarm/saham/StopAlarmReceiver.java
@@ -0,0 +1,12 @@
+package app.alarm.saham;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StopAlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AlarmMessagingService.stopLoopSound();
+    }
+}
