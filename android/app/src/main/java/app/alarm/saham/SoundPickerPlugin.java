745e216046803261c0958193f86b3e7fbeb6110c
javad2269 <155516010+javad2269@users.noreply.github.com>
2026-08-24 17:11:33 +0330

    Update SoundPickerPlugin.java

--- android/app/src/main/java/app/alarm/saham/SoundPickerPlugin.java
+++ android/app/src/main/java/app/alarm/saham/SoundPickerPlugin.java
@@ -5,12 +5,12 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 
-import com.getcapacitor.CapacitorPlugin;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 
-@CapacitorPlugin(name = "SoundPicker")   // ⭐ بسیار مهم
+// ⭐ annotation حذف شد - Capacitor از نام کلاس استفاده می‌کند
 public class SoundPickerPlugin extends Plugin {
     private static final int REQ_PICK = 9001;
     private PluginCall pendingCall = null;
